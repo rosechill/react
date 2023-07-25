7c92a7989d31fb27926ed744353a6c96363cda34
rosechill <bagasab11@gmail.com>
2023-07-25 19:12:55 +0700

    ScrollToTop

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import ScrollToTop from './components/scrollToTop.jsx'
+import ScrollToTop from './components/ScrollToTop.jsx'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './dist/css/main.css'
