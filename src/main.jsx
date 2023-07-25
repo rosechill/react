e068a32ab998a2c5f2394f8b7be0a85a2bd84b96
rosechill <bagasab11@gmail.com>
2023-07-25 19:34:42 +0700

    css

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import ScrollToTop from './components/ScrollToTop.jsx'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
-import './dist/css/main.css'
+import "./dist/css/main.css"
 import 'animate.css'
 
 import AOS from 'aos';
